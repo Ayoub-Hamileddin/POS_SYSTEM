dc2506e90cfdc95baebf69a091c23420677c6e1a
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-09-30 23:40:15 +0100

    extends from runtimeException instead of throwable

--- src/main/java/com/eyuup/exceptions/UserException.java
+++ src/main/java/com/eyuup/exceptions/UserException.java
@@ -1,6 +1,6 @@
 package com.eyuup.exceptions;
 
-public class UserException extends Throwable
+public class UserException extends RuntimeException 
 {
     public UserException (String message){
         super(message);
