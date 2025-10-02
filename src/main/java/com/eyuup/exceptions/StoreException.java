2fc353bdc5031d96fd9214cf641e2fa8e4bcf97f
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-02 22:24:32 +0100

    feat(store): add StoreException for handling store-related errors

---
+++ src/main/java/com/eyuup/exceptions/StoreException.java
@@ -0,0 +1,12 @@
+package com.eyuup.exceptions;
+
+
+
+
+public class StoreException extends RuntimeException  {
+
+    public StoreException(String message) {
+        super(message);
+    }
+        
+}
