3e0b8f924d0459cf0606fd0504fc669bf2564874
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-03 22:34:06 +0100

    Update StoreContact model: adjust fields and relationships

--- src/main/java/com/eyuup/modal/StoreContact.java
+++ src/main/java/com/eyuup/modal/StoreContact.java
@@ -2,12 +2,18 @@ package com.eyuup.modal;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 
 @Data
 @Embeddable
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoreContact {
 
 
