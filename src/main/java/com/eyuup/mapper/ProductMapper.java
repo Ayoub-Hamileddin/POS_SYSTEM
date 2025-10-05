47e01fa28c375df29c3699b0a7cdc5c02a2b3842
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-05 23:48:07 +0100

    Add ProductMapper for mapping between Product and ProductDTO

---
+++ src/main/java/com/eyuup/mapper/ProductMapper.java
@@ -0,0 +1,39 @@
+package com.eyuup.mapper;
+
+import com.eyuup.modal.Product;
+import com.eyuup.modal.Store;
+import com.eyuup.payload.dto.ProductDTO;
+
+public class ProductMapper {
+    public static ProductDTO ToDTO(Product product){
+        return ProductDTO.builder()
+
+                .id(product.getId())
+                .name(product.getName())
+                .sku(product.getSku())
+                .description(product.getDescription())
+                .mrp(product.getMrp())
+                .sellingPrice(product.getSellingPrice())
+                .brand(product.getBrand())
+                .image(product.getImage())
+                // .categoryId(product.getCategory().getId())
+                .storeId(product.getStore()!=null?product.getStore().getId():null)
+
+            .build();
+    }
+    public static Product ToEntity(ProductDTO productDTO,Store store){
+        return Product.builder()
+                .id(productDTO.getId())
+                .name(productDTO.getName())
+                .sku(productDTO.getSku())
+                .description(productDTO.getDescription())
+                .mrp(productDTO.getMrp())
+                .sellingPrice(productDTO.getSellingPrice())
+                .brand(productDTO.getBrand())
+                .image(productDTO.getImage())
+                .store(store)
+                // .category(productDTO.getCategoryId())
+                
+        .build();
+    }
+}
