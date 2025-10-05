13107d080dcba23ead2a71c52e38f56d9590a27a
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-05 23:48:11 +0100

    Define ProductService interface for product operations

---
+++ src/main/java/com/eyuup/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.eyuup.service;
+
+import java.util.List;
+
+import com.eyuup.modal.User;
+import com.eyuup.payload.dto.ProductDTO;
+
+public interface ProductService {
+
+    ProductDTO createProduct(ProductDTO productDTO , User user);
+
+    ProductDTO udpateProduct(Long ProductId,ProductDTO productDTO , User user);
+
+    void deleteProduct(Long ProductId , User user);
+
+    List<ProductDTO> getProductByStoreId(Long storeId);
+    
+    List<ProductDTO> searchByKeyword(Long storeId,String keyword);
+}
