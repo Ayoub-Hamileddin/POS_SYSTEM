d4a7780682436643760a26f9a4113512d28e4561
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-11 00:45:31 +0100

    Update ProductController: handle JWT correctly and improve update endpoint

--- src/main/java/com/eyuup/controllers/ProductController.java
+++ src/main/java/com/eyuup/controllers/ProductController.java
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eyuup.modal.User;
 import com.eyuup.payload.dto.ProductDTO;
+import com.eyuup.payload.response.ApiResponse;
 import com.eyuup.service.ProductService;
+import com.eyuup.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,32 +27,48 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
+
         private final ProductService  productService;
+        private final UserService userService;
+
+
 
         @PostMapping
         public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO , 
-                                                                        @RequestHeader String jwt)
+                                                                        @RequestHeader("Authorization") String jwt)
                 {
+                    User user=userService.getUserFromJwtToken(jwt);
                     return ResponseEntity.ok(
-                        productService.createProduct(productDTO, null)
+                        productService.createProduct(productDTO, user)
                     );
                 }
 
+
+
         @PutMapping("/{id}")
         public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id ,@RequestBody ProductDTO productDTO , 
-                                                                @RequestHeader String jwt)
+                                                                @RequestHeader("Authorization") String jwt)
         {
+            User user=userService.getUserFromJwtToken(jwt);
             return ResponseEntity.ok(
-                productService.udpateProduct(id,productDTO, null)
+                productService.udpateProduct(id,productDTO, user)
             );
         } 
 
 
+
+
         @DeleteMapping("/{id}")
-        public void deleteProduct(@PathVariable Long id, 
-                                                                @RequestHeader String jwt)
+        public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long id, 
+                                                                @RequestHeader("Authorization") String jwt)
         {
            productService.deleteProduct(id, null);
+
+           ApiResponse apiResponse=new ApiResponse();
+
+           apiResponse.setMessage("product with id: "+ id +" deleted successfuly");
+
+           return ResponseEntity.ok(apiResponse);
         } 
 
 
@@ -56,15 +76,18 @@ public class ProductController {
 
         @GetMapping("/store/{storeId}")
         public ResponseEntity<List<ProductDTO>> getProductByStoreId(@PathVariable Long storeId, 
-                                                                @RequestHeader String jwt)
+                                                                @RequestHeader("Authorization")  String jwt)
         {
             List<ProductDTO> productDTOs=productService.getProductByStoreId(storeId);
 
             return ResponseEntity.ok( productDTOs);
         } 
-        @GetMapping("/{storeId}")
-        public ResponseEntity<List<ProductDTO>> getProductByStoreId(@PathVariable Long storeId,@Param(value = "query") String keyword, 
-                                                                @RequestHeader String jwt)
+
+
+
+        @GetMapping("/search")
+        public ResponseEntity<List<ProductDTO>> getProductByKeyword(@RequestParam(value = "storeId") Long storeId,@RequestParam(value = "query") String keyword, 
+                                                                @RequestHeader("Authorization") String jwt)
         {
             List<ProductDTO> productDTOs=productService.searchByKeyword(storeId,keyword);
 
