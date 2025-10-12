ae2ffed3b120052f94607a3d4e815f7c7bd1ea31
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-12 03:59:00 +0100

    Add CategoryDTO for data transfer

---
+++ src/main/java/com/eyuup/payload/dto/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.eyuup.payload.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CategoryDTO {
+    private Long id;
+
+    private String name;
+    
+    private StoreDTO store;
+}
