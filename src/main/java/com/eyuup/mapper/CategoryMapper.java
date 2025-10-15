c0a9a702952ad3e95ef3e4b2b6defb545d03df1b
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-15 20:52:41 +0100

    refactor(CategoryMapper): adjust mapping logic for Category consistency

--- src/main/java/com/eyuup/mapper/CategoryMapper.java
+++ src/main/java/com/eyuup/mapper/CategoryMapper.java
@@ -3,26 +3,17 @@ package com.eyuup.mapper;
 
 
 import com.eyuup.modal.Category;
-import com.eyuup.modal.User;
 import com.eyuup.payload.dto.CategoryDTO;
 
 public class CategoryMapper {
+
     public static CategoryDTO ToDTO (Category category){
         return CategoryDTO.builder()
-
+            .id(category.getId())
             .name(category.getName())
             .storeId(category.getStore()!=null ? category.getStore().getId():null)
             
         .build();
     }
 
-    public static Category ToEntity(CategoryDTO categoryDTO,User user){
-    return Category.builder()
-        
-            .id(categoryDTO.getId())
-            .name(categoryDTO.getName())
-            .store(StoreMapper.toEntity(categoryDTO.getStore(),user.getStore().getStoreAdmin()))
-        
-        .build();
-    }
 }
