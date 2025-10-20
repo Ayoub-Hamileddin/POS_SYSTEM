f26fbb370a1fe5a4c127f36fd8c80a91d256d50f
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-20 21:33:32 +0100

    Update BranchRepository

--- src/main/java/com/eyuup/repository/BranchRepository.java
+++ src/main/java/com/eyuup/repository/BranchRepository.java
@@ -1,10 +1,12 @@
 package com.eyuup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eyuup.modal.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch,Long> {
     // getBranchByStoreId -> JPQL .
-    Branch findyStoreId(Long storeId);
+   List<Branch>findByStoreId(Long storeId);
 }
