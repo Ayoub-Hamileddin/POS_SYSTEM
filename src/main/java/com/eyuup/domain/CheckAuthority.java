764cccb782650f8678a09af63e8d70cb94e8bd64
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-20 23:38:39 +0100

    Update CheckAuthority logic for authorization

--- src/main/java/com/eyuup/domain/CheckAuthority.java
+++ src/main/java/com/eyuup/domain/CheckAuthority.java
@@ -1,22 +1,24 @@
 package com.eyuup.domain;
 
+import java.nio.file.AccessDeniedException;
+
 import com.eyuup.modal.Store;
 import com.eyuup.modal.User;
 
 public class CheckAuthority {
    
-    public static void isAuthorized(User user,Store store){
+    public static void isAuthorized(User user,Store store) throws Exception{
         
             boolean isAdmin=user.getRole().equals(UserRole.ROLE_ADMIN);
 
             boolean isManager=user.getRole().equals(UserRole.ROLE_STORE_MANAGER);
 
-            boolean isSameSite=user.equals(store.getStoreAdmin());
+            boolean isSameSite=user.getId().equals(store.getStoreAdmin().getId());
 
-            boolean isAuthorized=isSameSite &&(isAdmin || isManager);
+            boolean isAuthorized=isAdmin || (isManager && isSameSite);
 
             if (!isAuthorized) {
-                throw new SecurityException("you are not allowed to modify this item");
+                throw new AccessDeniedException("you are not allowed to modify this item");
             }
     }
 }
