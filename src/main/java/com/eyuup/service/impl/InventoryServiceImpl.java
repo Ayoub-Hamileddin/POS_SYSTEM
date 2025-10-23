653c521564f67bb5b4740d9c60f84b7eb385d21f
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-23 03:12:01 +0100

    Implement InventoryService in InventoryServiceImpl

---
+++ src/main/java/com/eyuup/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,107 @@
+package com.eyuup.service.impl;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.eyuup.mapper.InventoryMapper;
+import com.eyuup.modal.Branch;
+import com.eyuup.modal.Inventory;
+import com.eyuup.modal.Product;
+import com.eyuup.modal.Store;
+import com.eyuup.modal.User;
+import com.eyuup.payload.dto.InventoryDTO;
+import com.eyuup.repository.BranchRepository;
+import com.eyuup.repository.InventoryRepository;
+import com.eyuup.repository.ProductRepository;
+import com.eyuup.repository.StoreRepository;
+import com.eyuup.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryServiceImpl implements InventoryService {
+     
+    private final ProductRepository productRepository;
+    private final BranchRepository branchRepository;
+    private final StoreRepository storeRepository;
+    private final InventoryRepository inventoryRepository;
+    
+    @Override
+    public InventoryDTO createInventory(InventoryDTO inventoryDTO, User user) throws Exception {
+       Store store=storeRepository.findByStoreAdminId(user.getId()).orElseThrow(
+            ()->new AccessDeniedException("you are not the manager of this store")
+        );
+
+       Branch branch=branchRepository.findById(inventoryDTO.getBranchId()).orElseThrow(
+           ()->new Exception("barnch not found")
+        );
+
+       Product product=productRepository.findById(inventoryDTO.getProductId()).orElseThrow(
+           ()->new Exception("barnch not found")
+        );
+
+        Inventory inventory=InventoryMapper.toEntity(inventoryDTO,branch,product);
+
+        Inventory inventorySaved= inventoryRepository.save(inventory);
+
+        return InventoryMapper.toDTo(inventorySaved);
+    }
+
+    @Override
+    public InventoryDTO updateInventory(Long inventoryId, InventoryDTO inventoryDTO, User user) throws Exception {
+             Store store=storeRepository.findByStoreAdminId(user.getId()).orElseThrow(
+                    ()->new AccessDeniedException("you are not the manager of this store")
+            );
+
+
+            Inventory updatingInventory=inventoryRepository.findById(inventoryId).orElseThrow(
+                ()-> new Exception("inventory not found")
+            );
+
+            updatingInventory.setQuantity(inventoryDTO.getQuantity());
+            updatingInventory.setLastUpdatedTime(LocalDateTime.now());
+
+            return InventoryMapper.toDTo(inventoryRepository.save(updatingInventory));
+
+    }
+
+    @Override
+    public void delete(Long inventoryId, User user) throws Exception {
+        Store store=storeRepository.findByStoreAdminId(user.getId()).orElseThrow(
+            ()->new Exception("you are not the manager of this store ")
+        );
+
+        Inventory inventory=inventoryRepository.findById(inventoryId).orElseThrow(
+                ()-> new Exception("inventory not found")
+            );
+
+        inventoryRepository.delete(inventory);    
+        
+    }
+
+    @Override
+    public InventoryDTO getInventoriesById(Long inventoryId) throws Exception {
+
+        Inventory inventory=inventoryRepository.findById(inventoryId).orElseThrow(
+                ()-> new Exception("inventory not found")
+        );   
+        return InventoryMapper.toDTo(inventory);
+        
+        }
+
+    @Override
+    public InventoryDTO getInventoryByProductId(Long ProductId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInventoryByProductId'");
+    }
+
+    @Override
+    public InventoryDTO getInventoryByBranchId(Long BranchId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInventoryByBranchId'");
+    }
+
+}
