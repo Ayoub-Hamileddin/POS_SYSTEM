56611caa7fcbb2ce1e7d2e7aa3999940536973f8
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-28 03:33:35 +0100

    Update StoreRepository with recent modifications

--- src/main/java/com/eyuup/repository/StoreRepository.java
+++ src/main/java/com/eyuup/repository/StoreRepository.java
@@ -1,9 +1,11 @@
 package com.eyuup.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eyuup.modal.Store;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
-    Store findByStoreAdminId(Long adminId);
+   Optional<Store> findByStoreAdminId(Long adminId);
 }
