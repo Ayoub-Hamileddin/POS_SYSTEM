03a4db66966dc10e8fb9fb4a2eefda93d48d5aaa
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-12 19:59:51 +0100

    Update Employee service interface

--- src/main/java/com/eyuup/service/EmployeeService.java
+++ src/main/java/com/eyuup/service/EmployeeService.java
@@ -2,13 +2,20 @@ package com.eyuup.service;
 
 import java.util.List;
 
+import com.eyuup.modal.User;
 import com.eyuup.payload.dto.EmployeeDto;
 
 public interface EmployeeService {
-    EmployeeDto createEmployee(EmployeeDto employeeDto);
-    EmployeeDto updateEmployee(Long employeeId,EmployeeDto employeeDto);
-    EmployeeDto deleteEmployee(Long employeeId);
-    List<EmployeeDto> getAllEmployees();
-    EmployeeDto getEmployeeById(Long employeeId);
-    List<EmployeeDto> getEmployeeByBranchId(Long branchId);
+    
+    EmployeeDto createEmployee(EmployeeDto employeeDto,User user) throws Exception;
+
+    EmployeeDto updateEmployee(Long employeeId,EmployeeDto employeeDto,User user) throws Exception;
+
+    void deleteEmployee(Long employeeId,User user) throws Exception;
+
+    List<EmployeeDto> getAllEmployees(User user) throws Exception;
+
+    EmployeeDto getEmployeeById(Long employeeId,User user) throws Exception;
+
+    List<EmployeeDto> getEmployeeByBranchId(Long branchId,User user) throws Exception;
 }
